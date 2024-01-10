e3e2d9e0884faca16604bb25f8a6f8ca3148dc07
Isagani Lapira <lapiraisagani@gmail.com>
2024-01-10 13:13:17 +0800

    Adding: registration activity with creating account in firebase

---
+++ app/src/main/java/com/example/sustainabyte/RegistrationActivity.java
@@ -0,0 +1,63 @@
+package com.example.sustainabyte;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private MaterialButton btn_register;
+    private TextInputEditText et_email,et_password,et_confirm_password;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        initialize();
+        listener();
+    }
+
+    private void initialize() {
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+        et_confirm_password = findViewById(R.id.et_confirm_password);
+        btn_register = findViewById(R.id.btn_register);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    private void listener() {
+        btn_register.setOnClickListener(v->{
+            String email = String.valueOf(et_email.getText()).trim();
+            String password = String.valueOf(et_password.getText()).trim();
+            String confirmPass = String.valueOf(et_confirm_password.getText()).trim();
+
+            if(password.equals(confirmPass)) // register account
+                registerAccount(email,password);
+            else
+                Toast.makeText(this, "Password does not matched", Toast.LENGTH_LONG).show();
+
+        });
+    }
+
+    private void registerAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this,task -> {
+                    if(task.isSuccessful()){ //created successfully
+                        Toast.makeText(this, "Account successfully created", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(this,MainActivity.class);
+                        startActivity(intent); //go to login activity
+                    }else{
+                        Toast.makeText(this, "Error: "+
+                                        String.valueOf(task.getException().getMessage().toString()),
+                                        Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+}
