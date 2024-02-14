e8b9ae4998427a948d1fd43bd889df0aa662f3e5
Jeays Digo <jeaysdigo@outlook.com>
2024-02-14 15:14:11 +0800

    added navbar

--- app/src/main/java/com/example/sustainabyte/HomepageActivity.java
+++ app/src/main/java/com/example/sustainabyte/HomepageActivity.java
@@ -4,16 +4,43 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.sustainabyte.databinding.ActivityHomepageBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomepageActivity extends AppCompatActivity {
 
+    ActivityHomepageBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+        binding = ActivityHomepageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.homes) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.people) {
+                replaceFragment(new CommunityFragment());
+            } else if (item.getItemId() == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+
+        // Check if user is signed in
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            // If not signed in, go back to login page
+            startActivity(new Intent(this, MainActivity.class));
+            finish(); // Finish the current activity
+        }
     }
 
     @Override
@@ -22,10 +49,19 @@ public class HomepageActivity extends AppCompatActivity {
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        //check if already signed in
-        if(user == null) {
-            Intent intent = new Intent(this,MainActivity.class);
-            startActivity(intent); //go back to login page
+        // Check if user is signed in
+        if (user == null) {
+            // If not signed in, go back to login page
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent); // Go back to login page
+            finish(); // Finish the current activity
         }
+    }
+
+    private void replaceFragment(Fragment fr) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fr);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/sustainabyte/MainActivity.java
+++ app/src/main/java/com/example/sustainabyte/MainActivity.java
@@ -6,7 +6,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
